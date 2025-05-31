a0da94dadfb39c0b52c0cd5c87ace166b00f74c1
Alex Grönholm <alex.gronholm@nextday.fi>
2025-05-31 12:36:46 +0300

    Fixed test failures on Python 3.14
    Fixes #148.

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -374,7 +374,7 @@ class ExceptionGroupSplitTests(ExceptionGroupTestBase):
 class DeepRecursionInSplitAndSubgroup(unittest.TestCase):
     def make_deep_eg(self):
         e = TypeError(1)
-        for _ in range(10000):
+        for _ in range(150_000):
             e = ExceptionGroup("eg", [e])
         return e
 
